2f5a4da6e44be35a8acd958010cb785881166c25
Saurabh Yadav <104919948+SaurabhYadav100@users.noreply.github.com>
2023-10-31 22:58:47 +0530

    Update script.js

--- Registration_Form/script.js
+++ Registration_Form/script.js
@@ -1,22 +1,4 @@
-/*const registrationForm = document.getElementById('registrationForm');
-        const output = document.getElementById('output');
-        const outputFullName = document.getElementById('outputFullName');
-        const outputMobile = document.getElementById('outputMobile');
-        const outputEmail = document.getElementById('outputEmail');
-    
-        registrationForm.addEventListener('submit', function (e) {
-            e.preventDefault();
-
-            const fullName = document.getElementById('fullName').value;
-            const mobile = document.getElementById('mobile').value;
-            const email = document.getElementById('email').value;
-            outputFullName.textContent = fullName;          
-            outputMobile.textContent = mobile;
-            outputEmail.textContent = email;
-            output.style.display = 'block';
-        });*/
-
-      const registrationForm = document.getElementById('registrationForm');
+const registrationForm = document.getElementById('registrationForm');
 const outputData = document.getElementById('outputData');
 
 const enrolledStudents = []; // Array to store enrolled students' data
@@ -27,17 +9,12 @@ registrationForm.addEventListener('submit', function (e) {
     const fullName = document.getElementById('fullName').value;
     const email = document.getElementById('email').value;
     const number = document.getElementById('number').value;
-
-    // Create an object to represent a student
     const student = {
         fullName,
         email,
         number
     };
-
-    enrolledStudents.push(student); // Add the student to the array
-
-    // Display all enrolled students' information
+    enrolledStudents.push(student); 
     outputData.innerHTML = ''; // Clear the existing content
     enrolledStudents.forEach((student, index) => {
         const studentInfo = document.createElement('outputData');
@@ -46,12 +23,7 @@ registrationForm.addEventListener('submit', function (e) {
             <p><strong>Reg. No.:</strong> ${student.number}</p>
             <p><strong>Name:</strong> ${student.fullName}</p>
             <p><strong>Email:</strong> ${student.email}</p>
-            
        `;
         outputData.appendChild(studentInfo);
-        //outputData.style.display='block';
     });
-       //outputData.style.marginTop='50px';            
-    //
-   
 });
